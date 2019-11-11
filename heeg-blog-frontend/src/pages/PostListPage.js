21c5893e9e252e5749d7040e1c3e6f654c21131f
heeglee <heeg.aver@gmail.com>
2019-11-12 00:23:29 +0900

    11/11 header with responsive wrapper

--- heeg-blog-frontend/src/pages/PostListPage.js
+++ heeg-blog-frontend/src/pages/PostListPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Button from '../components/common/Button';
+import Header from '../components/common/Header';
 
 const PostListPage = () => {
     return (
         <div>
-            <Button>Button</Button>
+            <Header />
+            <div>HI</div>
         </div>
     );
 };
